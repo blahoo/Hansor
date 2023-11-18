810e1df058861b4d56415bd92f0ee4392f4eb7f4
blahoo <117117475+blahoo@users.noreply.github.com>
2023-11-18 16:39:00 -0500

    update

--- tracking.py
+++ tracking.py
@@ -6,6 +6,7 @@ import pyautogui
 
 pyautogui.FAILSAFE = False
 
+# setting up tkinker gui 
 root = tk.Tk()
 root.title("Hansor")
 
@@ -16,47 +17,60 @@ frame_window.pack(padx=10, pady=10)
 label = tk.Label(frame_window)
 label.pack()
 
-frame_scale = 1000
+# scale to resize camera capture
+frame_scale = 500
 
+# introduce mediapipe utils and detection model
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-hands = mp_hands.Hands(min_detection_confidence=0.1, min_tracking_confidence=0.60) 
+hands = mp_hands.Hands(min_detection_confidence=0.80, min_tracking_confidence=0.60) 
 
+# set up camera capture
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     raise Exception("Could not open video device")
 
+# scale hand coordinate position and move mouse cursor
 def move_cursor(x, y):
 
-    print(x, y)
+    # ([x/y] - min) / (max - min)
+    x = (x - 0.2) / (0.8 - 0.2)
+    y = (y - 0.2) / (0.5 - 0.2)
+    
+    # boundry for x and y value
+    if x <= 0: x = 0
+    if x >= 0.99: x = 0.99
+    if y <= 0: y = 0
+    if y >= 0.99: y = 0.99
 
     pyautogui.moveTo(x * pyautogui.size().width, y * pyautogui.size().height)
 
-
+# main method 
 def update_window():
     success, frame = cap.read()
     if success:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+        # process the image to RGB, rescale, and flip
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         h, w = frame.shape[:2]
         frame = cv2.resize(frame, (frame_scale, h*frame_scale//w))
-
         frame = cv2.flip(frame, 1)
 
+        # detect hand in image
         frame.flags.writeable = False
-        
         results = hands.process(frame)
-        
         frame.flags.writeable = True
-                    
+
+        # draw landmarks if hand detected and move cursor accordingly  
         if results.multi_hand_landmarks:
             for num, hand in enumerate(results.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
             
             move_cursor(results.multi_hand_landmarks[0].landmark[8].x, results.multi_hand_landmarks[0].landmark[8].y)
 
+        # convert image array to tkiner image and update window
         photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
         label.config(image=photo)
         
@@ -67,10 +81,10 @@ def update_window():
 
     label.after(10, update_window)  
 
+# call main loop
 update_window()
 
 root.mainloop()
-
 cap.release()
 
 
