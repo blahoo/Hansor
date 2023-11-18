cdb560fb14c79ec381b2bc6251a12799ca2711c9
blahoo <117117475+blahoo@users.noreply.github.com>
2023-11-18 16:07:13 -0500

    update

--- main.py
+++ main.py
@@ -4,60 +4,70 @@ import mediapipe as mp
 from PIL import Image, ImageTk
 import pyautogui
 
+pyautogui.FAILSAFE = False
+
 root = tk.Tk()
 root.title("Hansor")
 
-title = tk.Label(root, text="Live Webcam")
+title = tk.Label(root, text="Move Your Hand!")
 title.pack()
+frame_window = tk.Frame(root)
+frame_window.pack(padx=10, pady=10)
+label = tk.Label(frame_window)
+label.pack()
 
 frame_scale = 1000
 
-frame_window = tk.Frame(root)
-frame_window.pack(padx=10, pady=10)
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+hands = mp_hands.Hands(min_detection_confidence=0.1, min_tracking_confidence=0.60) 
 
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     raise Exception("Could not open video device")
 
-label = tk.Label(frame_window)
-label.pack()
+def move_cursor(x, y):
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+    print(x, y)
 
-def update_frame():
+    pyautogui.moveTo(x * pyautogui.size().width, y * pyautogui.size().height)
 
-    with mp_hands.Hands(min_detection_confidence=0.85, min_tracking_confidence=0.60) as hands: 
 
-        success, frame = cap.read()
-        if success:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+def update_window():
+    success, frame = cap.read()
+    if success:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            h, w = frame.shape[:2]
-            frame = cv2.resize(frame, (frame_scale, h*frame_scale//w))
+        h, w = frame.shape[:2]
+        frame = cv2.resize(frame, (frame_scale, h*frame_scale//w))
 
-            frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, 1)
 
-            frame.flags.writeable = False
+        frame.flags.writeable = False
+        
+        results = hands.process(frame)
+        
+        frame.flags.writeable = True
+                    
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
             
-            results = hands.process(frame)
-            
-            frame.flags.writeable = True
-                        
-            if results.multi_hand_landmarks:
-                for num, hand in enumerate(results.multi_hand_landmarks):
-                    mp_drawing.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS, mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4), mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2))
+            move_cursor(results.multi_hand_landmarks[0].landmark[8].x, results.multi_hand_landmarks[0].landmark[8].y)
 
-                pyautogui.moveTo(results.multi_hand_landmarks[0].landmark[8].x * pyautogui.size().width, results.multi_hand_landmarks[0].landmark[8].y * pyautogui.size().height)
+        photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
+        label.config(image=photo)
+        
+        label.image = photo
 
+    else:
+        exit(0)
 
-            photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
-            label.config(image=photo)
-            
-            label.image = photo
-        label.after(10, update_frame)  
-update_frame()
+    label.after(10, update_window)  
+
+update_window()
 
 root.mainloop()
 
