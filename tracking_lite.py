ab07b3d5254da3aaa6f0cc3ca7ded7f0b09b6434
blahoo <117117475+blahoo@users.noreply.github.com>
2023-11-19 19:23:48 -0500

    update

--- tracking_lite.py
+++ tracking_lite.py
@@ -1,10 +1,23 @@
 import tkinter as tk
 import cv2
 import mediapipe as mp
-from PIL import Image, ImageTk
 import pyautogui
 
 pyautogui.FAILSAFE = False
+
+# setting up tkinker gui 
+root = tk.Tk()
+root.title("Hansor_Lite (Tracking Only)")
+
+title = tk.Label(root, text="Move your hand in front of the camera,", font='Helvetica 10 bold')
+title.pack(padx=80, pady=5)
+sub_title = tk.Label(root, text="Your pointer finger is now your cursor!", font='Helvetica 10 bold')
+sub_title.pack()
+div = tk.Label(root, text="...")
+div.pack(pady=15)
+note = tk.Label(root, text="Powered by Google Mediapipe and Open CV - By GG", font="Helvetica 8")
+note.pack()
+
 
 # introduce mediapipe utils and detection model
 mp_drawing = mp.solutions.drawing_utils
@@ -31,33 +44,34 @@ def move_cursor(x, y):
     if y <= 0: y = 0
     if y >= 0.99: y = 0.99
 
-    print(f"({x}, {y})")
+    # print(f"({x}, {y})")
 
     pyautogui.moveTo(x * pyautogui.size().width, y * pyautogui.size().height)
 
 # main method 
 def main_loop():
-    while True:
-        success, frame = cap.read()
-        if success:
+    success, frame = cap.read()
+    if success:
 
-            # process the image to RGB and flip
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.flip(frame, 1)
+        # process the image to RGB and flip
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.flip(frame, 1)
 
-            # detect hand in image
-            frame.flags.writeable = False
-            results = hands.process(frame)
-            frame.flags.writeable = True
+        # detect hand in image
+        frame.flags.writeable = False
+        results = hands.process(frame)
+        frame.flags.writeable = True
 
-            # draw landmarks if hand detected and move cursor accordingly  
-            if results.multi_hand_landmarks:            
-                move_cursor(results.multi_hand_landmarks[0].landmark[8].x, results.multi_hand_landmarks[0].landmark[8].y)
-        else:
-            cap.release()
-            exit(0)
+        # draw l    andmarks if hand detected and move cursor accordingly  
+        if results.multi_hand_landmarks:            
+            move_cursor(results.multi_hand_landmarks[0].landmark[8].x, results.multi_hand_landmarks[0].landmark[8].y)
+        
+    else:
+        cap.release()
+        exit(0)
+    
+    root.after(10, main_loop)  
 
 main_loop()
-
-
-
+root.mainloop()
+cap.release()
