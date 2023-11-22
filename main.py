f9645211fd0d0299eef72c0d49e661fc28cde440
blahoo <117117475+blahoo@users.noreply.github.com>
2023-11-21 20:59:23 -0500

    update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageTk
 import pyautogui
 import time
 
+# set up tkinter gui
 root = tk.Tk()
 root.title("Hansor")
 
@@ -16,24 +17,18 @@ frame_scale = 1000
 frame_window = tk.Frame(root)
 frame_window.pack(padx=10, pady=10)
 
-label = tk.Label(frame_window)
-label.pack()
+live_window = tk.Label(frame_window)
+live_window.pack()
 
-
+# set up video webcame capture
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     raise Exception("Could not open video device")
 
-
-
+# set up mediapip utils
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-
-
-detection_confidence_level = 0.8
-tracking_confidence_level = 0.5
-
 
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
 BaseOptions = mp.tasks.BaseOptions
@@ -41,15 +36,19 @@ RunningMode = mp.tasks.vision.RunningMode
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 
+# min and max confidence
+detection_confidence_level = 0.8
+tracking_confidence_level = 0.5
+
+# function to handle model output
 last_action = time.time()
 
 def result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
     global last_action
 
     try:
-        print(result.gestures[0][0].category_name, result.hand_landmarks[0][8].x, result.hand_landmarks[0][8].y)
-
-        print(time.time())
+        #print(result.gestures[0][0].category_name, result.hand_landmarks[0][8].x, result.hand_landmarks[0][8].y)
+        #print(time.time())
 
         if result.gestures[0][0].category_name == "Pointing_Up":
             move_cursor(result.hand_landmarks[0][8].x, result.hand_landmarks[0][8].y)
@@ -64,13 +63,8 @@ def result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms
         return
 
 
-
 def left_click():
-
-    x, y = pyautogui.position()
-
-    pyautogui.leftClick(x, y)
-
+    pyautogui.leftClick(pyautogui.position())
 
 def move_cursor(x, y):
 
@@ -85,11 +79,10 @@ def move_cursor(x, y):
     if y >= 0.99: y = 0.99
 
     # print(f"({x}, {y})")
-
     pyautogui.moveTo(x * pyautogui.size().width, y * pyautogui.size().height)
 
 
-
+# import model and define mediapipe configuration
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path='model.task'),
     running_mode=RunningMode.LIVE_STREAM,
@@ -98,8 +91,6 @@ options = GestureRecognizerOptions(
     min_tracking_confidence=tracking_confidence_level,
     result_callback=result)
 
-
-
 with mp.tasks.vision.GestureRecognizer.create_from_options(options) as recog:
 
     stamp = 0
@@ -107,28 +98,25 @@ with mp.tasks.vision.GestureRecognizer.create_from_options(options) as recog:
     def main_loop():
         _, frame = cap.read()
 
+        # preprocess image
         image = cv2.flip(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), 1)
         mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data = image)
 
         global stamp
-
         stamp +=1
 
+        # asynchronously process image
         recog.recognize_async(mp_image, stamp)
 
+        # convert image to tkinter image and update live feed frame
         photo = ImageTk.PhotoImage(image=Image.fromarray(image))
-        label.config(image=photo)
+        live_window.config(image=photo)
 
-        label.image = photo
-        print(1)
-
-        label.after(100, main_loop)  
-
+        live_window.image = photo
+        live_window.after(100, main_loop)  
 
     main_loop()
-
     root.mainloop()
-
     cap.release()
 
 
